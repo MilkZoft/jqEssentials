8bf7713354720c48d7eb86746d40e11a7381546f
Carlos Santana <czantany@MacBook-Pro-de-Carlos.local>
2016-02-24 00:35:31 -0800

    Adding documentation to jqEssentials

--- src/jqEssentials.js
+++ src/jqEssentials.js
@@ -1,11 +1,41 @@
-'use strict';
-
+/**
+ * jqEssentials JavaScript Library v0.0.1
+ * https://github.com/MilkZoft/jqEssentials
+ *
+ * Copyright (c) 2016 Carlos Santana
+ * Released under the MIT license
+ * https://github.com/MilkZoft/jqEssentials/blob/master/LICENSE
+ */
 var jqEssentials = {};
 
 (function() {
+    'use strict';
+
+    /**
+     * Initializes the $ function.
+     * @param {string} selector Query Selector
+     */
     var init = function(selector) {
+        // Gets the element (by id) or the elements (by class)
         var el = selector[0] === '.' ? document.querySelectorAll(selector) : document.querySelector(selector);
 
+        // Filters
+        var filters = {
+            slug: function(str) {
+                var from = 'ãàáäâẽèéëêìíïîõòóöôùúüûñç·/_,:;';
+                var to   = 'aaaaaeeeeeiiiiooooouuuunc------';
+
+                str = str.replace(/^\s+|\s+$/g, '').toLowerCase();
+
+                for (var i = 0, l = from.length; i < l; i++) {
+                    str = str.replace(new RegExp(from.charAt(i), 'g'), to.charAt(i));
+                }
+
+                return str.replace(/[^a-z0-9 -]/g, '').replace(/\s+/g, '-').replace(/-+/g, '-');
+            }
+        };
+
+        // Public Methods
         return {
             addClass: addClass,
             each: each,
@@ -13,33 +43,50 @@ var jqEssentials = {};
             val: val
         };
 
+        /**
+         * Adds a class to an element
+         * @param {string} className Name of the class
+         */
         function addClass(className) {
             if (!el.classList.contains(className)) {
                 el.className += ' ' + className;
             }
         }
 
+        /**
+         * Iterates over elements
+         * @param {function} callback Callback action
+         */
         function each(callback) {
-            console.log('Dentro de each', el);
             var node;
             var i;
 
             for (i = 0; i < el.length; i++) {
                 node = el[i];
-                callback.call(node, i, node)
+                callback.call(node, i, node);
             }
 
             return this;
         }
 
+        /**
+         * Attach events
+         * @param {string} event Event type (click, keyup, etc.)
+         * @param {function} handler Function that will be executed for that event
+         */
         function on(event, handler) {
             el.addEventListener(event, handler, false);
         }
 
+        /**
+         * Gets and sets a value applying optional filters
+         * @param {string} event Event type (click, keyup, etc.)
+         * @param {function} handler Function that will be executed for that event
+         */
         function val(value, filter) {
             if (value) {
-                if (filter === 'slug') {
-                    el.value = slug(value);
+                if (filter) {
+                    el.value = filters[filter](value);
                 } else {
                     el.value = value;
                 }
@@ -47,26 +94,14 @@ var jqEssentials = {};
                 return el.value;
             }
         }
-
-        /// Private functions ///
-        function slug(str) {
-            var from = 'ãàáäâẽèéëêìíïîõòóöôùúüûñç·/_,:;';
-            var to   = 'aaaaaeeeeeiiiiooooouuuunc------';
-
-            str = str.replace(/^\s+|\s+$/g, '').toLowerCase();
-
-            for (var i = 0, l = from.length; i < l; i++) {
-                str = str.replace(new RegExp(from.charAt(i), 'g'), to.charAt(i));
-            }
-
-            return str.replace(/[^a-z0-9 -]/g, '').replace(/\s+/g, '-').replace(/-+/g, '-');
-        }
     }
 
+    // Creating new instance
     jqEssentials = function(selector) {
         return new init(selector)
     }
 
+    // If there is no jQuery we take the $ function
     if (!window.$) {
         window.$ = jqEssentials;
     }
